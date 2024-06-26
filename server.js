56267de3b873d8031ab776e4150ffab00663f4fe
ABAC <yunanwu123@gmail.com>
2024-06-26 07:10:47 +0700

    setup cookie, session

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const db = require('./database');
 const hbs = require('hbs');
 const path = require('path')
 const {rootRoutes}= require('./routes/routes');
+const cookieParser = require("cookie-parser");
+const session = require('express-session');
 
 dotenv.config({ path: path.join(__dirname, './environment/.env') });
 const server = express()
@@ -17,6 +19,14 @@ hbs.registerPartials(partialpath)
 
 server.use(express.json())
 server.use(express.urlencoded({ extended: false }))
+server.use(cookieParser())
+
+server.use(session({
+    secret: 'bromount', // Replace with a strong secret for session encryption
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false } // Set secure to true if using HTTPS
+}));
 
 server.use(express.static(publcpath))
 
