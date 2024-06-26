57feb69ca72eaaeabd89bf062fb6901e119bdb04
ABAC <yunanwu123@gmail.com>
2024-06-26 07:11:52 +0700

    added auth for session and token 2in1

---
+++ auth/verifySession.js
@@ -0,0 +1,9 @@
+const verifySession = (req, res, next) => {
+  if (req.session && req.session.username) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+module.exports = { verifySession }
