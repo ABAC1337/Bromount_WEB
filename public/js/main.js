c56de0ab8a399db22987c3e110372281d7763dfb
cantikaokzen <cantikaputri022020@gmail.com>
2024-07-23 21:59:25 +0700

    make hbs and css file

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,48 @@
+$(document).ready(function () {
+  $("#owl-demo").owlCarousel({
+    loop: true,
+    items: 5, //10 items above 1000px browser width
+    itemsDesktop: [1000, 4], //5 items between 1000px and 901px
+    itemsDesktopSmall: [900, 2], // betweem 900px and 601px
+    itemsTablet: [600, 2], //2 items between 600 and 0;
+    itemsMobile: false, // itemsMobile disabled - inherit from itemsTablet option
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 2,
+        nav: true,
+      },
+      600: {
+        items: 3,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+        loop: true,
+      },
+    },
+  });
+});
+function showWarning(id) {
+  document.getElementById('delete-id').value = id;
+  document.getElementById('warning').style.display = 'block';
+}
+function showTable(id){
+  document.getElementById('update').value = id;
+  document.getElementById('edit').style.display='block';
+}
+function ShowWarningDelete(id){
+  document.getElementById('delete-id').value = id;
+  document.getElementById('warning').style.display ='block'
+}
+
+function updateOrderDate() {
+  const orderDate = document.getElementById('order-date').value;
+  document.getElementById('add-order-date').value = orderDate;
+  document.getElementById('add-cart-date').value = orderDate;
+}
+
 function showHome(){
     document.getElementById('home').style.display='block'
     document.getElementById('cart').style.display='none'
