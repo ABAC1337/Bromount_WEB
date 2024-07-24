b48bde6ee73f85506bea6df465c8c726222d6921
ABAC <yunanwu123@gmail.com>
2024-07-24 16:14:12 +0700

    added libraries (multer, connect-flash)

--- server.js
+++ server.js
@@ -1,40 +1,58 @@
-const dotenv = require('dotenv');
-const express = require('express');
-const db = require('./database');
-const hbs = require('hbs');
-const path = require('path')
-const {rootRoutes}= require('./routes/routes');
+const dotenv = require("dotenv");
+const express = require("express");
+const db = require("./database");
+const hbs = require("hbs");
+const path = require("path");
+const { rootRoutes } = require("./routes/routes");
 const cookieParser = require("cookie-parser");
-const session = require('express-session');
+const session = require("express-session");
+const flash = require("connect-flash");
 
-dotenv.config({ path: path.join(__dirname, './.env') });
-const server = express()
-const publcpath = path.join(__dirname,'./public')
-const viewspath = path.join(__dirname, "./template/views")
-const partialpath = path.join(__dirname, './template/partials')
+dotenv.config({ path: path.join(__dirname, "./environment/.env") });
+const server = express();
+const publcpath = path.join(__dirname, "./public");
+const viewspath = path.join(__dirname, "./template/views");
+const partialpath = path.join(__dirname, "./template/partials");
 
-server.set('view engine', 'hbs')
-server.set('views',viewspath)
-hbs.registerPartials(partialpath)
+server.set("view engine", "hbs");
+server.set("views", viewspath);
+hbs.registerPartials(partialpath);
+hbs.registerHelper("formatCurrency", function(value) {
+  if (typeof value === "number") {
+    return new hbs.SafeString(value.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' }));
+  }
+  return value;
+});
 
-server.use(express.json())
-server.use(express.urlencoded({ extended: false }))
-server.use(cookieParser())
 
-server.use(session({
-    secret: 'bromount', // Replace with a strong secret for session encryption
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(cookieParser());
+server.use(flash());
+
+server.use(
+  session({
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false } // Set secure to true if using HTTPS
-}));
+    cookie: { secure: false },
+  })
+);
 
-server.use(express.static(publcpath))
+server.use(express.static(publcpath));
 
+// Middleware to set no-cache headers for sensitive routes
+server.use((req, res, next) => {
+  res.header(
+    "Cache-Control",
+    "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0"
+  );
+  next();
+});
 
 server.use(rootRoutes);
 
-
 db.connect().then(() => {
-    server.listen(process.env.PORT)
-    console.log(`Server running on http://localhost:${process.env.PORT}`)
-})
+  server.listen(process.env.PORT);
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
+});
