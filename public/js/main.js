19e4a3e1f518991d3add54f0a14c63223ab8ba7e
niko <nikoagustio22@gmail.com>
2024-07-26 21:22:13 +0700

    edit

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,36 @@
-$(document).ready(function () {
-  $("#owl-demo").owlCarousel({
-    loop: true,
-    items: 5, //10 items above 1000px browser width
-    itemsDesktop: [1000, 4], //5 items between 1000px and 901px
-    itemsDesktopSmall: [900, 2], // betweem 900px and 601px
-    itemsTablet: [600, 2], //2 items between 600 and 0;
-    itemsMobile: false, // itemsMobile disabled - inherit from itemsTablet option
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 2,
-        nav: true,
-      },
-      600: {
-        items: 3,
-        nav: false,
-      },
-      1000: {
-        items: 3,
-        nav: false,
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Initialize Owl Carousel
+    var owl = document.querySelector('#owl-demo');
+    if (owl) {
+      $(owl).owlCarousel({
         loop: true,
-      },
-    },
+        items: 5, // 10 items above 1000px browser width
+        itemsDesktop: [1000, 4], // 5 items between 1000px and 901px
+        itemsDesktopSmall: [900, 2], // between 900px and 601px
+        itemsTablet: [600, 2], // 2 items between 600px and 0;
+        itemsMobile: false, // itemsMobile disabled - inherit from itemsTablet option
+        responsiveClass: true,
+        responsive: {
+          0: {
+            items: 2,
+            nav: true,
+          },
+          600: {
+            items: 3,
+            nav: false,
+          },
+          1000: {
+            items: 3,
+            nav: false,
+            loop: true,
+          },
+        },
+      });
+    }
   });
-});
+
+
 function showWarning(id) {
   document.getElementById('delete-id').value = id;
   document.getElementById('warning').style.display = 'block';
